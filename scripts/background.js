9f9eda71d4526a93a82ff254f81f929d8504f6b5
kk69njit <kk69@njit.edu>
2024-10-06 23:22:04 -0400

    Initial controls implementation

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,45 @@
+const lang = "en-US";
+var initialRate = 1.0;
+
 // This listener listens for TTS requests from the button press in the extension popup.
 chrome.runtime.onMessage.addListener((request) => {
   if (request.text) {
-    tts(request.text);
+    chrome.storage.local.get(["rate"], (result) => {
+      const currentRate = result.rate || initialRate;
+      tts(request.text, currentRate);
+    });
+  }
+
+  if (request.slow) {
+    chrome.storage.local.get(["rate"], (result) => {
+      var currentRate = result.rate || initialRate;
+      currentRate = Math.max(0.5, currentRate - 0.1); // Prevent speaking rate from going too slow (half the speed)
+      chrome.storage.local.set({ rate: currentRate }, () => {
+        chrome.tts.isSpeaking((speaking) => {
+          if (speaking) {
+            chrome.tts.update({ rate: currentRate });
+          }
+        });
+      });
+    });
+  }
+
+  if (request.fast) {
+    chrome.storage.local.get(["rate"], (result) => {
+      var currentRate = result.rate || initialRate;
+      currentRate = Math.min(2.0, currentRate + 0.1); // Prevent speaking rate from going too fast (double the speed)
+      chrome.storage.local.set({ rate: currentRate }, () => {
+        chrome.tts.isSpeaking((speaking) => {
+          if (speaking) {
+            chrome.tts.update({ rate: currentRate });
+          }
+        });
+      });
+    });
+  }
+
+  if (request.stop) {
+    chrome.tts.stop();
   }
 });
 
@@ -16,13 +54,27 @@ chrome.runtime.onInstalled.addListener(() => {
 
 // Handle the click event for the context menu option.
 chrome.contextMenus.onClicked.addListener((menuOption) => {
-  if (menuOption.menuItemId === "ttsRightClick") tts(menuOption.selectionText);
+  if (menuOption.menuItemId === "ttsRightClick")
+    chrome.storage.local.get(["rate"], (result) => {
+      const currentRate = result.rate || initialRate;
+      tts(menuOption.selectionText, currentRate);
+    });
 });
 
 // This function is calling Chrome's TTS API to read the text aloud.
-function tts(text) {
-  const lang = "en-US";
-  const rate = 1.0;
-
-  chrome.tts.speak(text, { lang: lang, rate: rate });
+function tts(text, rate) {
+  chrome.tts.speak(text, {
+    requiredEventTypes: ["cancelled", "end", "interrupted"],
+    onEvent: function (event) {
+      if (
+        event.type === "cancelled" ||
+        event.type === "end" ||
+        event.type === "interrupted"
+      ) {
+        chrome.runtime.sendMessage({ ttsEnded: true });
+      }
+    },
+    lang: lang,
+    rate: rate,
+  });
 }
--- scripts/readmate.js
+++ scripts/readmate.js
@@ -1,11 +1,75 @@
+var isPaused = false;
+var textExists = false;
+
 // This is adding click functionality to the button in the in the extension popup.
 document.getElementById("ttsBtn").addEventListener("click", () => {
+  chrome.tts.isSpeaking((speaking) => {
+    if (speaking) {
+      if (isPaused) {
+        // Resume the speech if it was paused
+        chrome.tts.resume();
+        isPaused = false;
+        document.getElementById("ttsBtn").textContent = "Pause";
+      } else {
+        // Pause the speech if it's currently speaking
+        chrome.tts.pause();
+        isPaused = true;
+        document.getElementById("ttsBtn").textContent = "Resume";
+      }
+    } else {
+      // Start reading aloud if nothing is currently speaking
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id },
+          function: getHighlightedText,
+        });
+      });
+
+      if (textExists) {
+        document.getElementById("ttsBtn").textContent = "Pause";
+      }
+    }
+  });
+});
+
+document.getElementById("slowBtn").addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.scripting.executeScript({
       target: { tabId: tabs[0].id },
-      function: getHighlightedText,
+      function: () => {
+        chrome.runtime.sendMessage({ slow: true });
+      },
     });
   });
+});
+
+document.getElementById("fastBtn").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      function: () => {
+        chrome.runtime.sendMessage({ fast: true });
+      },
+    });
+  });
+});
+
+document.getElementById("stopBtn").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      function: () => {
+        chrome.runtime.sendMessage({ stop: true });
+      },
+    });
+  });
+});
+
+// Listen for messages from the background script to reset the button
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.ttsEnded) {
+    resetTTSButton();
+  }
 });
 
 // This function gets the highlighted text and sends it to the background service worker.
@@ -13,5 +77,12 @@ function getHighlightedText() {
   const selectedText = window.getSelection().toString();
   if (selectedText) {
     chrome.runtime.sendMessage({ text: selectedText });
+    textExists = true;
   }
+}
+
+// This function resets the primary TTS Button text to "Read Aloud"
+function resetTTSButton() {
+  document.getElementById("ttsBtn").textContent = "Read Aloud";
+  isPaused = false;
 }
