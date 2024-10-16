cd389d4833793ffe45604f2672cc948bb4ebd129
kk69njit <kk69@njit.edu>
2024-10-16 01:56:12 -0400

    Volume Slider Implemented

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,6 @@
 const lang = "en-US";
 var initialRate = 1.0;
+var initialVolume = 1.0;
 var chunks = [];
 var currentChunkIndex = 0;
 var sentenceSkipped = false;
@@ -7,10 +8,11 @@ var sentenceSkipped = false;
 // This listener listens for TTS requests from the button press in the extension popup.
 chrome.runtime.onMessage.addListener((request) => {
   if (request.text) {
-    chrome.storage.local.get(["rate"], (result) => {
+    chrome.storage.local.get(["rate", "volume"], (result) => {
       const currentRate = result.rate || initialRate;
+      const currentVolume = result.volume || initialVolume;
       chunks = request.text.match(/[^.!?;:]+[.!?;:]+/g) || [request.text]; // Split text into sentences using . ! ? ; : as delimiters.
-      tts(currentRate);
+      tts(currentRate, currentVolume);
     });
   }
 
@@ -32,24 +34,6 @@ chrome.runtime.onMessage.addListener((request) => {
     }
   }
 
-  // **TODO: Remove this once rate changing is implemented in settings.**
-  // if (request.rateChange) {
-  //   rateChanged = true;
-  //   chrome.storage.local.get(["rate"], (result) => {
-  //     var currentRate = result.rate || initialRate;
-  //     currentRate = request.slow
-  //       ? Math.max(0.5, currentRate - 0.1) // Prevent speaking rate from going too slow (half the speed of normal is lowest)
-  //       : Math.min(2.0, currentRate + 0.1); // Prevent speaking rate from going too fast (double the speed of normal is highest)
-  //     chrome.storage.local.set({ rate: currentRate }, () => {
-  //       chrome.tts.isSpeaking((speaking) => {
-  //         if (speaking) {
-  //           restartWithNewRate(currentRate);
-  //         }
-  //       });
-  //     });
-  //   });
-  // }
-
   if (request.stop) {
     resetTTS();
   }
@@ -67,29 +51,31 @@ chrome.runtime.onInstalled.addListener(() => {
 // Handle the click event for the context menu option.
 chrome.contextMenus.onClicked.addListener((menuOption) => {
   if (menuOption.menuItemId === "ttsRightClick")
-    chrome.storage.local.get(["rate"], (result) => {
+    chrome.storage.local.get(["rate", "volume"], (result) => {
       const currentRate = result.rate || initialRate;
+      const currentVolume = result.volume || initialVolume;
 
       // Split text into sentences using . ! ? ; : as delimiters.
       chunks = menuOption.selectionText.match(/[^.!?;:]+[.!?;:]+/g) || [
         menuOption.selectionText,
       ];
-      tts(currentRate);
+      tts(currentRate, currentVolume);
     });
 });
 
 // This function is calling Chrome's TTS API to read the text aloud.
-function tts(rate) {
+function tts(rate, volume) {
   if (currentChunkIndex < chunks.length) {
     chrome.tts.speak(chunks[currentChunkIndex], {
       requiredEventTypes: ["cancelled", "end", "interrupted", "error", "word"],
       lang: lang,
       rate: rate,
+      volume: volume,
       onEvent: function (event) {
         if (event.type === "end") {
           currentChunkIndex++;
           if (currentChunkIndex < chunks.length) {
-            tts(rate);
+            tts(rate, volume);
           }
         }
         if (event.type === "cancelled" || event.type === "interrupted") {
@@ -105,9 +91,10 @@ function tts(rate) {
 
 // This function restarts TTS with the from a different chunk.
 function restartTTS() {
-  chrome.storage.local.get(["rate"], (result) => {
+  chrome.storage.local.get(["rate", "volume"], (result) => {
     const rate = result.rate || initialRate;
-    tts(rate);
+    const volume = result.volume || initialVolume;
+    tts(rate, volume);
   });
 }
 
--- scripts/settings.js
+++ scripts/settings.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const rateSlider = document.getElementById("rate");
   const rateValue = document.getElementById("rateValue");
 
+  const volumeSlider = document.getElementById("volume");
+  const volumeValue = document.getElementById("volumeValue");
+
   // Load saved rate from chrome.storage to display the user's currently saved rate.
   chrome.storage.local.get("rate", function (result) {
     const savedRate = result.rate || 1.0; // Default rate is 1.0 if nothing is saved
@@ -10,13 +13,30 @@ document.addEventListener("DOMContentLoaded", function () {
     rateValue.textContent = savedRate;
   });
 
-  // Update the displayed value when the slider changes.
+  // Load saved volume from chrome.storage to display the user's currently saved volume.
+  chrome.storage.local.get("volume", function (result) {
+    const savedVolume = result.volume || 1.0; // Default volume is 1.0 if nothing is saved
+    volumeSlider.value = savedVolume;
+    volumeValue.textContent = savedVolume;
+  });
+
+  // Update the displayed value when the rate slider changes.
   rateSlider.addEventListener("input", function () {
     rateValue.textContent = rateSlider.value;
+  });
+
+  // Update the displayed value when the volume slider changes.
+  volumeSlider.addEventListener("input", function () {
+    volumeValue.textContent = volumeSlider.value;
   });
 
   // Save rate to chrome.storage when user changes slider.
   rateSlider.addEventListener("change", function () {
     chrome.storage.local.set({ rate: parseFloat(rateSlider.value) });
+  });
+
+  // Save volume to chrome.storage when user changes slider.
+  volumeSlider.addEventListener("change", function () {
+    chrome.storage.local.set({ volume: parseFloat(volumeSlider.value) });
   });
 });
